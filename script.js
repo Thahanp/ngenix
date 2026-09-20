35c8ea97aa2cbf789bcbaeb0b95d6124041991aa
Ubuntu <ubuntu@ip-172-31-4-155.ap-south-1.compute.internal>
2026-09-20 14:08:19 +0000

    files added

---
+++ script.js
@@ -0,0 +1,7 @@
+function changeText() {
+    document.getElementById("message").innerHTML =
+        "You clicked the button!";
+}function changeText() {
+    document.getElementById("message").innerHTML =
+        "You clicked the button!";
+}
